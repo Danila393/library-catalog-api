d3736972443085233ee5cfd5348b366c995b28c7
Данила <danil.anayev@mail.ru>
2026-07-12 18:27:22 +0300

    dev: config.py (pydantic validation)

---
+++ src/library_catalog/core/config.py
@@ -0,0 +1,47 @@
+from typing import Literal
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    app_name: str = "Library Catalog API"
+    environment: Literal["development", "staging", "production"]
+    debug: bool
+
+    # БД
+    database_url: PostgresDsn
+    database_pool_size: int = 20
+
+    # API
+    api_v1_prefix: str = "/api/v1"
+    log_level: str = "INFO"
+    docs_url: str = "/docs"
+    redoc_url: str = "/redoc"
+    cors_origins: list[str] = ["*"]
+
+    # Внешние API
+    openlibrary_base_url: str = "https://openlibrary.org"
+    openlibrary_timeout: float = 10.0
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False
+    )
+
+    @property
+    def is_production(self) -> bool:
+        return self.environment == "production"
+
+
+    @property
+    def database_url_str(self) -> str:
+        return str(self.database_url)
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
---
+++ src/library_catalog/core/database.py
@@ -0,0 +1,37 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from .config import settings
+
+
+class Base(DeclarativeBase):
+    """ Тут в будущем будут таблицы"""
+    pass
+
+
+engine = create_async_engine(
+    settings.database_url_str,
+    pool_size=settings.database_pool_size,
+    echo=settings.debug
+)
+
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+# Dependency
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback() # Отменяем изменения в БД, если что-то пошло не так
+            raise
+        finally:
+            await session.close() # Закрываем соединение чтобы не перегружать БД
+
+
